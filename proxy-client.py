722330a525f3bf05c54f673ab3e9d4b61d75e1eb
Nelson <ingnelson@yahoo.com>
2020-08-03 20:30:18 -0600

    Update proxy-client.py

--- proxy-client.py
+++ proxy-client.py
@@ -5,7 +5,7 @@ server_address = ('172.245.22.211', 80)
 client_socket.connect(server_address)
 
 request_header = 'GET #0=fown&method=1&down=10# HTTP/1.1\r\n ٌُُُُُُّّّّْْْْْْْْْْْْْْْْْْ\r\n\r\n
-client_socket.send(request_header)
+client_socket.send(request_header.encode('utf-8'))
 
 response = ''
 while True:
